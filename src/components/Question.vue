c17dedde6743bf8f27afb53a5dce0b330cb73e8a
Jake Prem <jakeprem@gmail.com>
2017-12-14 01:58:16 -0500

    Pull question out to its own component

---
+++ src/components/Question.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col">
+              <div class="card" style="width: 20rem;">
+                  <div class="card-block">
+                    <h4 class="card-title">{{question.title}}</h4>
+                    <p class="card-text">{{question.description}}</p>
+                    <table>
+                      <tr v-for="link in question.links" :key="link.title">
+                        <td>
+                          <a href="#" class="btn btn-primary" @click="loadLink(link.dest)">{{link.title}}</a>
+                          <a :href="link.dest">{{link.title}}</a>
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+          </div>
+</template>
+
+<script>
+export default {
+    name: 'Question',
+    props: ['question']
+}
+</script>
+
+<style>
+
+</style>
--- src/components/QuestionCardList.vue
+++ src/components/QuestionCardList.vue
@@ -1,30 +1,20 @@
 <template>
   <div class="container">
         <div class="row">
-          <div class="col" v-for="question in questions" :key="question.title">
-              <div class="card" style="width: 20rem;">
-                  <div class="card-block">
-                    <h4 class="card-title">{{question.title}}</h4>
-                    <p class="card-text">{{question.description}}</p>
-                    <table>
-                      <tr v-for="link in question.links" :key="link.title">
-                        <td>
-                          <a href="#" class="btn btn-primary" @click="loadLink(link.dest)">{{link.title}}</a>
-                          <a :href="link.dest">{{link.title}}</a>
-                        </td>
-                      </tr>
-                    </table>
-                  </div>
-                </div>
-          </div>
+          <question v-for="(question, index) in questions" :key="index" :question="question"></question>
         </div>
       </div>
 </template>
 
 <script>
+import Question from '@/components/Question.vue'
+
 export default {
   name: 'QuestionCardList',
-  props: ['questions']
+  props: ['questions'],
+  components: {
+    Question
+  }
 }
 </script>
 
