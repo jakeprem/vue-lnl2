a46ea6e78b4d172de68c586f1c8e1cf81816386c
Jake Prem <jakeprem@gmail.com>
2017-12-14 02:26:41 -0500

    Add basic vuex store

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+    test: true
+}
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+export default new Vuex.Store({
+    state,
+    getters,
+    actions,
+    mutations,
+    strict: false
+});
